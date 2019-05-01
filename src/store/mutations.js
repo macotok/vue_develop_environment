e266815e95d1a5cbcbde1ee6da86b6fe3fba4cd3
macotok <monochromer@r8.ucom.ne.jp>
2019-05-02 00:20:36 +0900

    fix eslint

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,10 +2,12 @@ import { SAMPLE_ACTION01, SAMPLE_ACTION02 } from './actions';
 
 const mutations = {
   [SAMPLE_ACTION01](state, inputData) {
-    state.label01 = inputData;
+    const data = state;
+    data.label01 = inputData;
   },
   [SAMPLE_ACTION02](state, inputData) {
-    state.label02 = inputData;
+    const data = state;
+    data.label02 = inputData;
   },
 };
 
