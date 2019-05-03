bf789dbff8b3618c3eeabdf4b79555fd699124a9
macotok <monochromer@r8.ucom.ne.jp>
2019-05-03 13:09:46 +0900

    add test code to emit

--- __tests__/components/atoms/form/AtTextInput.js
+++ __tests__/components/atoms/form/AtTextInput.js
@@ -2,15 +2,32 @@ import { shallowMount } from '@vue/test-utils';
 import AtTextInput from '@/components/atoms/form/AtTextInput';
 
 describe('AtTextInput', () => {
-  const wrapper = shallowMount(AtTextInput);
-  describe('v-on', () => {
-    test('inputでinputTextメソッドが実行されること', () => {
-      const stub = jest.fn();
-      wrapper.setMethods({
-        inputText: stub,
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(AtTextInput);
+  });
+  describe('template', () => {
+    describe('v-on', () => {
+      test('inputでinputTextメソッドが実行されること', () => {
+        const stub = jest.fn();
+        wrapper.setMethods({
+          inputText: stub,
+        });
+        wrapper.trigger('input');
+        expect(stub).toHaveBeenCalled();
       });
-      wrapper.trigger('input');
-      expect(stub).toHaveBeenCalled();
+    });
+  });
+  describe('methods', () => {
+    describe('inputText', () => {
+      test('input-textイベントがemitされること', () => {
+        wrapper.vm.inputText({ target: { value: 'inputValue' } });
+        expect(wrapper.emitted('input-text')).not.toBeUndefined();
+      });
+      test('input-textイベントのemitで値が渡されること', () => {
+        wrapper.vm.inputText({ target: { value: 'inputValue' } });
+        expect(wrapper.emitted('input-text')[0][0]).toBe('inputValue');
+      });
     });
   });
 });
